a556d98339afc4f1b6ebfe04d119b2f810c7df7a
itaicas21 <itaicas21@gmail.com>
2021-06-08 20:06:57 +0300

    Initial commit

---
+++ HeadlessScraperNew.js
@@ -0,0 +1,70 @@
+const puppeteer = require("puppeteer");
+const main = async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--proxy-server=socks5://127.0.0.1:9050"],
+  });
+  let link = "http://nzxj65x32vh2fkhk.onion/all";
+  const page = await browser.newPage();
+  while (link) {
+    console.log(await scrape(browser, link));
+    link = await nextPage(browser, link);
+  }
+  return browser.close();
+};
+
+const scrape = async (browser, link) => {
+  const page = await browser.newPage();
+  await page.goto(link, {
+    waitUntil: "load",
+    timeout: 0,
+  });
+  const postLinks = await page.evaluate(() =>
+    Array.from(document.querySelectorAll(".col-sm-12"))
+      .filter((postclass) => {
+        if (postclass.childNodes.length === 7) return true;
+      })
+      .map((post) => post.querySelector(".btn.btn-success"))
+      .map((btn) => btn.href)
+  );
+  const promiseArray = postLinks.map(async (link) => {
+    const newPage = await browser.newPage();
+    await newPage.goto(link, {
+      waitUntil: "load",
+      timeout: 0,
+    });
+    const titles = await newPage.evaluate(() =>
+      document
+        .querySelector(".col-sm-5")
+        .textContent.trim()
+        .trim()
+        .replace("\n", "")
+        .replace("\t", " ")
+        .replaceAll("\t", "")
+    );
+    await newPage.close();
+    return titles;
+  });
+
+  const titlesArray = await Promise.all(promiseArray);
+  await page.close();
+  return titlesArray;
+};
+async function nextPage(browser, link) {
+  const page = await browser.newPage();
+  await page.goto(link, {
+    waitUntil: "load",
+    timeout: 0,
+  });
+  const nextPage = await page.evaluate(() => {
+    const nodeList = Array.from(
+      document.querySelectorAll(".col-sm-12")
+    );
+    const navBar = nodeList[nodeList.length - 1];
+    const links = navBar.querySelectorAll("li");
+    return links[links.length - 1].querySelector("a")?.href;
+  });
+  await page.close();
+  return nextPage;
+}
+main();
---
+++ README.md
@@ -0,0 +1,2 @@
+# IntSights-Challenge
+ 
